3dd6ca9c711a10f50c09358d0dc57c8108803d10
uzairimran2009-blip <uzair.imran2009@gmail.com>
2026-05-13 10:59:44 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,44 +5,21 @@ import axios from "axios";
 function App() {
   const [carts, setCarts] = useState([]);
 
-  const cartsApiData = async () => {
+  const cartsApiData = async() => {
     try {
-      const cartsApiresp = await axios.get("htps://dummyjson.com/carts");
-      setCarts(cartsApiresp.data.carts);
-      console.log(cartsApiresp.data.carts);
-    } catch (error) {
-      console.log("Error:", error);
+      const cartsApiResp = await axios.get("https://dummyjson.com/carts");
+      setCarts(cartsApiResp.data.carts);
+      console.log(cartsApiResp.data.carts);
+    } catch (err) {
+      console.log("Error", err);
     }
   };
 
   useEffect(() => {
-    cartsApiData();
+    cartsApiData()
   }, []);
 
   return (
-    <div className="App">
-      <h1>Carts</h1>
-      {carts.map((cart) => (
-        <div key={cart.id} className="cart">
-          <h2>Cart ID: {cart.id}</h2>
-          <p>User ID: {cart.userId}</p>
-          <p>Total Products: {cart.totalProducts}</p>
-          <p>Total Quantity: {cart.totalQuantity}</p>
-          <p>Total Price: ${cart.total}</p>
-          <div className="products">
-            <h3>Products:</h3>
-            {cart.products.map((product) => (
-              <div key={product.id} className="product">
-                <p>Product ID: {product.id}</p>
-                <p>Title: {product.title}</p>
-                <p>Price: ${product.price}</p>
-                <p>Quantity: {product.quantity}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
-    </div>
   );
 }
 
